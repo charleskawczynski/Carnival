23271a41587ea7071afc493719d0aa58e1c2b954
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-02-18 11:26:49 -0800

    Add foo

--- src/Carnival.jl
+++ src/Carnival.jl
@@ -2,4 +2,6 @@ module Carnival
 
 greet() = print("Hello World!")
 
+foo() = 1
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Carnival
 
 @testset "My test" begin
     @test 1 + 1 == 2
+    @test Carnival.foo() == 1
 end
 
